e6d117833c7929f59c7e56ad95026fdaea5cabf5
maslina524 <luk.kirill2804@gmail.com>
2026-05-03 12:55:18 +0300

    added cli args parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "gdvc")]
+#[command(about = "git for Geometry Dash levels", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(external_subcommand)]
+    Other(Vec<String>),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    
+    let status: Result<(), String> = match cli.command {
+        Commands::Other(args) => {
+            let cmd_name = args.get(0).unwrap();
+            Err(format!("gdvc: `{cmd_name}` is not a gdvc command."))
+        }
+    };
+
+    if let Err(e) = status {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
 }
