60572c596d7105429cfd7e5821335efc04bf0b79
maslina524 <luk.kirill2804@gmail.com>
2026-05-04 23:41:52 +0300

    added an implementation for the destroy action

--- src/cmds.rs
+++ src/cmds.rs
@@ -1,13 +1,14 @@
 use std::time::SystemTime;
-use std::fs::File;
-use std::io::Write;
+use std::fs::{File, self};
+use std::io::{Write, stdin, stdout};
 
 use sha2::{Sha256, Digest};
 use hex;
 
 use crate::ws::WsClient;
-use crate::level::{self, set_marker};
+use crate::level::{self, get_marker, set_marker};
 use crate::files;
+use crate::consts::{YELLOW_COLOR, ESC_COLOR};
 
 pub fn help() {
     println!("usage: gdvc <command> [<args>]\n");
@@ -40,11 +41,55 @@ pub fn init() -> Result<(), String> {
     Ok(())
 }
 
+pub fn destroy(force: bool, hard: bool) -> Result<(), String> {
+    let mut ws = WsClient::connect()?;
+
+    let mut string = ws.get_level_string()?;
+
+    let marker = match get_marker(&string) {
+        Some(m) => m,
+        None => return Err("The level is not initialized.".to_string())
+    };
+
+    if !force {
+        println!("This action will remove all gdvc tracking from your level.");
+        println!("This operation is irreversible.\n");
+
+        if hard {
+            println!("[!] With --hard, this will also permanently delete the entire");
+            println!("    level directory for this level from your disk.");
+            println!("    All commits will be lost forever.\n");
+        }
+
+        println!("Type {}YES{} to confirm:", YELLOW_COLOR, ESC_COLOR);
+        let mut input = String::new();
+        let _ = stdout().flush();
+        stdin().read_line(&mut input).map_err(|_| "Did not enter a correct string")?;
+        input = input.trim().to_string();
+
+        if input != "YES" {
+            return Ok(());
+        }
+    }
+
+    if hard {
+        let path = files::get_level_path(marker);
+        let ret = fs::remove_dir_all(&path);
+    }
+    
+    let new_string = set_marker(&mut string, 0);
+    let _ = ws.replace_level_string(&new_string);
+
+    let _ = ws.disconnect();
+
+    Ok(())
+}
+
 pub fn commit(message: &String) -> Result<(), String> {
     let mut ws = WsClient::connect()?;
 
     let string = ws.get_level_string()?;
-    let marker = level::get_marker(&string).ok_or("An uninitialized level.".to_string())?;
+    let marker = level::get_marker(&string).ok_or("The level is not initialized.".to_string())?;
 
     files::create_level_folder(marker)?;
 
