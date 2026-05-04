e1b31a6b915f46ccc149111f67a4e816eded9f34
maslina524 <luk.kirill2804@gmail.com>
2026-05-04 17:43:59 +0300

    added create_level_folder

--- src/files.rs
+++ src/files.rs
@@ -1,4 +1,6 @@
 use std::path::PathBuf;
+use std::fs;
+
 use dirs::data_local_dir;
 
 pub fn get_gdvc_path() -> PathBuf {
@@ -7,6 +9,18 @@ pub fn get_gdvc_path() -> PathBuf {
 
 pub fn get_level_path(marker: u32) -> PathBuf {
     get_gdvc_path().join(marker.to_string())
+}
+
+pub fn create_level_folder(marker: u32) -> Result<(), String> {
+    let path = get_level_path(marker);
+
+    fs::create_dir_all(&path)
+        .map_err(|e| format!("Failed to create base level directory: {}", e))?;
+
+    fs::create_dir_all(&path.join("commits"))
+        .map_err(|e| format!("Failed to create commits directory: {}", e))?;
+
+    Ok(())
 }
 
 #[cfg(test)]
