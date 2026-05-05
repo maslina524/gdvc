f3e6f636f7f6c242967b404a97b7b208cb22428a
maslina524 <luk.kirill2804@gmail.com>
2026-05-05 23:03:56 +0300

    help action impl has been moved to actions

---
+++ src/actions/help.rs
@@ -0,0 +1,14 @@
+pub fn run() {
+    println!("usage: gdvc [-v | --version] [-p | --path]");
+    println!("            <command> [<args>]");
+
+    println!("\nstart a working area");
+    println!("    init        Initialize your level for Gdvc");
+    println!("    destroy     Remove all Gdvc tracking");
+
+    println!("\nexamine the history and state");
+    println!("    log         Show commit logs");
+
+    println!("\nwork on the current");
+    println!("    commit      Record the changes");
+}
