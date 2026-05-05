8a9c308218b327dca0f5079b2405626ef37ad2a3
maslina524 <luk.kirill2804@gmail.com>
2026-05-05 15:16:09 +0300

    changed colors in consts.rs

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,7 @@
 pub const SECRET_KEY: &str = "kA26";
 
-pub const YELLOW_COLOR: &str = "\x1b[1;33m";
-pub const BLUE_COLOR: &str = "\x1b[1;34m";
+pub const YELLOW_COLOR: &str = "\x1b[0;33m";
+pub const BLUE_COLOR: &str = "\x1b[1;36m";
 pub const ESC_COLOR: &str = "\x1b[0m";
 
 pub const VERSION: &str = "0.1.0";
