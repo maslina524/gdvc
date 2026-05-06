6d380d426d295fc13344d70df7f1970ca1fa7508
maslina524 <luk.kirill2804@gmail.com>
2026-05-06 16:59:04 +0300

    first release!!!!

--- src/consts.rs
+++ src/consts.rs
@@ -4,4 +4,4 @@ pub const YELLOW_COLOR: &str = "\x1b[0;33m";
 pub const BLUE_COLOR: &str = "\x1b[1;36m";
 pub const ESC_COLOR: &str = "\x1b[0m";
 
-pub const VERSION: &str = "0.1.0";
+pub const VERSION: &str = "1.0.0";
