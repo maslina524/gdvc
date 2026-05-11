eff0a38420eb519fea5d48ba4d833222f1caf078
maslina524 <luk.kirill2804@gmail.com>
2026-05-11 11:07:28 +0300

    changed version in consts.rs

--- src/consts.rs
+++ src/consts.rs
@@ -5,6 +5,6 @@ pub const BLUE: &str = "\x1b[1;36m";
 pub const INVERT: &str = "\x1b[0;30;47m";
 pub const ESC: &str = "\x1b[0m";
 
-pub const VERSION: &str = "1.0.0";
+pub const VERSION: &str = "1.0.1";
 
 pub const LINES_THRESHOLD: usize = 14;
