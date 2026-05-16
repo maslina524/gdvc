fc74b91cd71b5bea6202d7d26944f48b6c0d6c57
maslina524 <luk.kirill2804@gmail.com>
2026-05-16 15:22:13 +0300

    added BOLD to consts.rs

--- src/consts.rs
+++ src/consts.rs
@@ -13,6 +13,7 @@ pub const YELLOW: &str = "\x1b[0;33m";
 pub const BLUE: &str = "\x1b[1;36m";
 pub const INVERT: &str = "\x1b[0;30;47m";
 pub const ESC: &str = "\x1b[0m";
+pub const BOLD: &str = "\x1b[1m";
 
 pub const VERSION: &str = "1.0.1";
 
