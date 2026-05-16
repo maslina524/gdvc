e869c3802f466f912a53b83b81613e1dfe915949
maslina524 <luk.kirill2804@gmail.com>
2026-05-16 15:22:59 +0300

    the hashes of the old and new level strings are now printed

--- src/actions/diff.rs
+++ src/actions/diff.rs
@@ -1,11 +1,13 @@
 use std::collections::HashSet;
 use std::fs;
+use std::slice::ChunkBy;
 
 use crate::files::get_level_path;
-use crate::level::{decode_string, get_marker};
+use crate::level::{self, decode_string, get_marker};
 use crate::actions::commit::read_commit_string;
 
 use crate::ws::WsClient;
+use crate::consts::{BOLD, ESC};
 
 pub fn run() -> Result<(), String> {
     let mut ws = WsClient::connect()?;
@@ -18,20 +20,26 @@ pub fn run() -> Result<(), String> {
 
     let head_hash = fs::read_to_string(get_level_path(marker).join("HEAD"))
         .map_err(|e| format!("Failed to read the HEAD file: {e}"))?;
-    let path = get_level_path(marker).join("commits").join(head_hash);
+    let path = get_level_path(marker).join("commits").join(&head_hash);
 
     let head_string = read_commit_string(path)
         .map_err(|e| format!("Failed to read the HEAD commit string: {e}"))?;
     let decoded_string = decode_string(&head_string)?;
 
     let old_sep = decoded_string.find(";").unwrap();
-    let old_objects = decoded_string[old_sep..].to_string();
+    let old_string = decoded_string[old_sep..].to_string();
 
     let new_sep = string.find(";").unwrap();
-    let new_objects = string[new_sep..].to_string();
+    let new_string = string[new_sep..].to_string();
 
-    let (added, removed) = compare_strings(&new_objects, &old_objects)?;
+    let (added, removed) = compare_strings(&new_string, &old_string)?;
 
+    let cur_hash = level::get_string_hash(&string);
+
+    println!(
+        "{}index {}..{}{}",
+        BOLD, &head_hash[..7], &cur_hash[..7], ESC
+    );
     println!("{} insertions(+), {} deletions(-)", added, removed);
 
     let _ = ws.disconnect();
