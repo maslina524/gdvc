1d69c1f5951d037f1cc269c6c9b6b59868ac593e
maslina524 <luk.kirill2804@gmail.com>
2026-05-27 11:25:45 +0300

    added convert docs to adoc & html (asciidoctor)

--- convert-doc-page.py
+++ convert-doc-page.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 import re
+import subprocess
 
 ESC = "\x1b[0m"
 BOLD = "\x1b[1m"
@@ -8,6 +9,10 @@ ITALIC = "\x1b[3m"
 
 def re_line(line: str) -> str:
     ret = re.sub(r'`(.*?)`', rf'{ITALIC}\1{ESC}', line, flags=re.DOTALL)
+    return ret
+
+def re_line_adoc(line: str) -> str:
+    ret = re.sub(r'`(.*?)`', rf'__\1__', line, flags=re.DOTALL)
     return ret
 
 def save_to_txt(data: str, path: Path) -> None:
@@ -34,10 +39,35 @@ def save_to_txt(data: str, path: Path) -> None:
                 ret.append(f"{re_line(line)}\n")
     
     output = "".join(ret)
-    with open(f"{path.name[:-3]}.txt", "w") as f:
+    with open(f"doc/txt/{path.name[:-3]}.txt", "w") as f:
         f.write(output)
 
-    print(output)
+def save_to_html(data: str, path: Path) -> None: # use asciidoc
+    lines = data.split("\n")
+    ret = []
+    is_code = False
+    for line in lines:
+        if line.startswith("# "):
+            ret.append(f"= {line[2:]}\n")
+
+        elif line.startswith("## "):
+            ret.append(f"\n== {line[3:]}\n")
+
+        elif line.startswith("```"):
+            is_code = not is_code
+            
+        else:
+            if line != "":
+                if is_code:
+                    ret.append(f"`{re_line_adoc(line)}`\n")
+                else:
+                    ret.append(f"{re_line_adoc(line)}\n")
+    
+    output = "".join(ret)
+    with open(f"doc/adoc/{path.name[:-3]}.adoc", "w") as f:
+        f.write(output)
+
+    subprocess.run(f"asciidoctor -o doc/html/{path.name[:-3]}.html doc/adoc/{path.name[:-3]}.adoc", shell=True)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -50,3 +80,4 @@ if __name__ == "__main__":
         data = f.read()
 
     save_to_txt(data, input_file)
+    save_to_html(data, input_file)
