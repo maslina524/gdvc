19b7466cc3cbc4153f279ed70bed9769a929debd
maslina524 <luk.kirill2804@gmail.com>
2026-05-27 11:43:57 +0300

    added a cmd_handler for opening doc about a subcommand

--- src/actions/help.rs
+++ src/actions/help.rs
@@ -1,4 +1,12 @@
-pub fn run() {
+use std::path::PathBuf;
+
+use std::process::Command;
+
+pub fn run(command: Option<String>) -> Result<(), String> {
+    if let Some(cmd) = command {
+        cmd_handler(&cmd)?;
+        return Ok(());
+    }
     println!("usage: gdvc [-v | --version] [-p | --path]");
     println!("            <command> [<args>]");
 
@@ -14,4 +22,30 @@ pub fn run() {
     println!("\nwork on the current");
     println!("    commit      Record the changes");
     println!("    rollback    Restore the level to a previous commit");
+
+    Ok(())
+}
+
+fn cmd_handler(cmd: &str) -> Result<(), String> {
+    let target = "html";
+    let path_str = format!("./doc/{target}/{cmd}.{target}");
+    let path = PathBuf::from(&path_str);
+
+    if !path.exists() {
+        return Err(format!("fatal: '{path_str}': documentation file not found."));
+    }
+
+    #[cfg(target_os = "windows")]
+    let status = Command::new("cmd").args(&["/C", "start", &path_str]).status();
+    
+    #[cfg(target_os = "macos")]
+    let status = Command::new("open").arg(&path_str).status();
+    
+    #[cfg(target_os = "linux")]
+    let status = Command::new("xdg-open").arg(&path_str).status();
+
+    match status {
+        Ok(_) => Ok(()),
+        Err(e) => Err(format!("failed to open file: {}", e)),
+    }
 }
